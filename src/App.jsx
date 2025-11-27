63f73a7b98f4fec235626f4e3d0ece1a9a238779
habibur-rahman-tanvir <habibur353637@gmail.com>
2025-11-28 00:41:58 +0600

    Just test git and github

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,22 @@
+import { useState } from "react";
+
 const App = () => {
+  const [count, setCount] = useState(0);
   return (
-    <div>
-      <h1 className="text-2xl font-bold text-center">Hello Blog post</h1>
+    <div className="flex items-center justify-center min-h-screen">
+      <div className="p-2 space-y-2.5 shadow-md shadow-gray-500 bg-gray-200 rounded-md">
+        <h1 className="text-5xl font-bold text-center opacity-70 ">
+          Count: {count}
+        </h1>
+        <div className="flex justify-center">
+          <button
+            className="px-3 py-2 text-2xl font-semibold text-white uppercase bg-blue-600 active:bg-blue-800 active:scale-95 outline-0 rounded-2xl"
+            onClick={() => setCount(count + 1)}
+          >
+            Increase
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
